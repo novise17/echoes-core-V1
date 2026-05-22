0a85379cb10ae9d3900f3857b485de7fc6d3cc2d
novise17 <koffin@nycstudents.net>
2026-05-22 11:19:29 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
+import { startGameLoop } from "./engine/gameLoop.js";
+import { Fighter } from "./fighters/Fighter.js";
+import { initInput } from "./engine/input.js";
 
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 1000;
+canvas.height = 600;
+
+export const gameState = {
+  player1: new Fighter(100, 300, "red"),
+  player2: new Fighter(700, 300, "blue")
+};
+
+initInput();
+
+startGameLoop(canvas, ctx, gameState);
