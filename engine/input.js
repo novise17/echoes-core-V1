8bb931864ccdde054ff180c7ff3d83ab260c6f09
novise17 <koffin@nycstudents.net>
2026-05-22 11:20:17 -0400

    Update input.js

--- engine/input.js
+++ engine/input.js
@@ -1,1 +1,11 @@
+export const keys = {};
 
+export function initInput() {
+  window.addEventListener("keydown", (e) => {
+    keys[e.key] = true;
+  });
+
+  window.addEventListener("keyup", (e) => {
+    keys[e.key] = false;
+  });
+}
