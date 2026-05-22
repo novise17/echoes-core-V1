1734b4ef78980e4ecb211c9bddfcfcdab1726f4e
novise17 <koffin@nycstudents.net>
2026-05-22 11:19:51 -0400

    Update gameLoop.js

--- engine/gameLoop.js
+++ engine/gameLoop.js
@@ -1,1 +1,22 @@
+export function startGameLoop(canvas, ctx, state) {
+  function loop() {
+    update(state, canvas);
+    draw(ctx, state);
+    requestAnimationFrame(loop);
+  }
 
+  loop();
+}
+
+function update(state, canvas) {
+  state.player1.update(canvas, state.player2);
+  state.player2.update(canvas, state.player1);
+}
+
+function draw(ctx, state) {
+  ctx.fillStyle = "#111";
+  ctx.fillRect(0, 0, 1000, 600);
+
+  state.player1.draw(ctx);
+  state.player2.draw(ctx);
+}
